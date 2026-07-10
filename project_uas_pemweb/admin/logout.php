da689ad8729c5ce08296eb0cc99559ff87098bf8
madityamuhklish <madityamuhklish@student.uir.ac.id>
2026-07-10 17:20:52 +0700

    Delete project_uas_pemweb directory

--- project_uas_pemweb/admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) session_start();
-
-unset($_SESSION['admin_id'], $_SESSION['admin_name'], $_SESSION['admin_email'], $_SESSION['is_admin']);
-
-header("Location: login.php");
-exit;
